adc3c7003d89805f398194617191a7584b6fdc16
GiftUzochi <uzochigift1999@gmail.com>
2023-10-15 09:45:18 -0700

    binary

--- print_binary.c
+++ print_binary.c
@@ -7,20 +7,19 @@
 
 int print_binary (va_list element)
 {
-	unsigned int b = va_arg element
-		unsigned int value = 1
+	unsigned int b = va_arg element;
+		unsigned int value = 1;
 		int num = length (98);
-	int b = 0
-		int i = 1
-		if b == 0
+	int b = 0;
+		int i = 1;
+		if (b == 0)
 			_putchar('0')
 				return 1;
 }
 if (int b = 0)
 	_putchar('0');
-	b++
 {
 for (value = 1, b < 1, b++)
 	_putchar(num[1] + '0');
 }
-return(length)
+return(length);
