609c3c5f8253ad28c8625edecd7379d9216c86f0
GiftUzochi <uzochigift1999@gmail.com>
2023-10-16 15:12:16 -0700

    field

--- print_field_width.c
+++ print_field_width.c
@@ -15,5 +15,36 @@ while (format[i])
 	if (format[i] = '%')
 		i++;
 int fieldwidth = 0;
-
+while (format[i] >= '0' && format[i] <= '9');
+	fieldwidth = fieldwidth * 10 + (format[i] - '0');
+	i++;
+}
+if (format[i] == 'd' || format[i] == 's' || format[i] == 'f')
+{
+	if (format[i] == 'd')
+	{
+		int value = va_arg(args, int);
+		printf("%d", fieldwidth, value);
+	}
+	else if (format[i] == "s")
+	{
+		char str = va_arg(args, char);
+		printf("%s", fieldwidth, str);
+	}
+	else if (format[i] == "f")
+	{
+		double value = va_arg9args, double);
+		printf("%f", fieldwidth, value);
+	}
+}
+else
+{
+	_putchar(format[i]);
+}
+i++
+}
+va_end(args);
+}
+return 0;
+}
 
