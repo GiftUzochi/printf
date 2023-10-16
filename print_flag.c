357656b271b35a81460cfa7878c595ba858ae29b
GiftUzochi <uzochigift1999@gmail.com>
2023-10-16 15:46:42 -0700

    flag

---
+++ print_flag.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_flag - function to indicate that numeric values
+ * @element: the va_list element passed in from _printf.c
+ * Return: the number of characters in the string
+ */
+
+int print_flag(va_list element)
+{
+
